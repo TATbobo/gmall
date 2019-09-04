6883e57200f2ea438f6f270c54117dccaff10335
tucker <1179468360@qq.com>
2019-09-04 17:38:06 +0800

    9.4:gmall骨架搭建，dubbo整合

---
+++ gmall-user-service/src/test/java/com/tucker/gmall/userservice/GmallUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tucker.gmall.userservice;
+
+
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
+public class GmallUserServiceApplicationTests {
+
+   /* @Test*/
+    public void contextLoads() {
+    }
+
+}
